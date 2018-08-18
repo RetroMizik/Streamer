4c212bd9f90e42a34d93f50360dd138a60e6ead9
LandoSansNom <cherlanphilippe@gmail.com>
2018-08-18 17:00:02 -0400

    Work on the Login

--- app/src/main/java/com/codepath/streamer/Timeline.java
+++ app/src/main/java/com/codepath/streamer/Timeline.java
@@ -8,6 +8,6 @@ public class Timeline extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_timeline);
+        setContentView(R.layout.activity_login);
     }
 }
